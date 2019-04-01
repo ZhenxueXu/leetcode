98fadedf8d260426279ebea9e93b165fcd9503f4
ZhenxueXu <zhenxuexu@gmail.com>
2019-04-01 18:54:17 +0800

    add: max-points-on-a-line

--- src/solution/MaxPointsOnALine.java
+++ src/solution/MaxPointsOnALine.java
@@ -17,23 +17,24 @@ public class MaxPointsOnALine {
 		for (int i = 0; i < points.length - 1; i++) {
 			for (int j = i + 1; j < points.length; j++) {
 				int num = 2;
+				// 两点构成一条直线，然后遍历其余的点是否在这条直线上
 				for (int k = 0; k < points.length; k++) {
 					if (k == i || k == j){
 						continue;
 					}
+					// 两点重合时，构成的直线只有一个点，找出其余重合点便为该直线上的点数量，如{[1,1],[1,1],[1,1],[1,3]},在一条直线上的最大点数为3
 					if (equal(points[i],points[j])){
 						if (equal(points[i],points[k])) {
 							num++;
 						}
 						continue;
 					}
+					// 两点不重合是，判断其余点是否与i，j两点在一条直线上
 					if ((points[i].y - points[k].y) * (points[j].x - points[k].x) == (points[j].y - points[k].y) * (points[i].x - points[k].x)){
 						num++;
 					}
 				}
-				if (num > maxPoins) {
-					maxPoins = num;
-				}
+				maxPoins = Math.max(maxPoins,num);
 			}
 		}
 		return maxPoins;
